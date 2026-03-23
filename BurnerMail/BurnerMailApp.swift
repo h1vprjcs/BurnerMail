b75545e8a43bcc2cca2d280da7407239b7055b8b
Shiv Patel <shiv0050401@gmail.com>
2026-03-23 15:12:29 -0400

    Initial Commit

---
+++ BurnerMail/BurnerMailApp.swift
@@ -0,0 +1,17 @@
+//
+//  BurnerMailApp.swift
+//  BurnerMail
+//
+//  Created by Shiv Patel on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BurnerMailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
